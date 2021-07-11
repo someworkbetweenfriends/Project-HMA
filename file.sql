9817ab9598f760e6357c0034781fd831a440d8ef
Armaanilyas <77886146+Armaanilyas@users.noreply.github.com>
2021-07-11 20:55:18 +0100

    Add files via upload
    Start of Database

---
+++ file.sql
@@ -0,0 +1,11 @@
+char *sql = "DROP TABLE IF EXISTS Logins:"
+	CREATE TABLE "Logins" (Id INT,
+Username TEXT, Salt  TEXT, Hash TEXT) :
+	INSERT INTO "Logins" VALUES(1,
+'Ahmed' , 'Alpine' , '123456'):"
+INSERT INTO "Logins" VALUES(2,
+'Armaan' , 'Alpine', '123456')" ,
+char * err_message:
+returnCode = sqlite3_exec(dataBasw, sql ,0 , 0 ,
+&err_messsage:
+printf("Return value is %i (%s) \n" , returnCode, err_message): 
